4ef15370ba19ce3bc4de6e4380a7ad6250383351
elvisonob <elvisonob@yahoo.com>
2024-11-18 07:41:02 +0000

    Todo App completed

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,7 +1,6 @@
 import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRequest } from './actions/index.js';
-
 import TodoList from './component/TodoList.js';
 import TodoText from './component/TodoText.js';
 import Notification from './UI/Notification.js';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Use 'react-dom' instead of 'react-dom/client'
+import ReactDOM from 'react-dom';
 import TodoApp from './TodoApp.js';
 import { Provider } from 'react-redux';
 import store from './store/index';
